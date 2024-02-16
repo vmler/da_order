cb7344e9877adca22d56a99b85e3dfd04ec568b4
byldh <byldh124@gmail.com>
2024-02-16 10:14:52 +0900

    create project

---
+++ android/app/src/main/kotlin/com/moondroid/da_order/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.moondroid.da_order
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const PRIMARY_COLOR = Color(0xff22a45d);
+const BODY_TEXT_COLOR = Color(0xff868686);
+const INPUT_BG_COLOR = Color(0xfffbfbfb);
+const INPUT_BORDER_COLOR = Color(0xfff3f2f2);
